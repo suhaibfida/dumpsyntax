e7856d6d8f1c7ba69efa72d89962a2575947f1da
suhaibfida <suhaibfida33@outlook.com>
2026-01-22 23:33:41 +0530

    add database user schema

--- apps/httpbackend/src/controller/controller.ts
+++ apps/httpbackend/src/controller/controller.ts
@@ -1,4 +1,3 @@
-const signup = () => {
-  console.log("hello");
-};
+import { Request, Response } from "express";
+const signup = (req: Request, res: Response) => {};
 export default signup;
--- apps/httpbackend/src/main.ts
+++ apps/httpbackend/src/main.ts
@@ -2,14 +2,16 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import router from "./router/router";
+import "dotenv/config";
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: process.env.FRONTEND_URL || "http://localhost:3000",
+    credentials: true,
   }),
 );
-app.use("api/v1", router);
+app.use("/api/v1", router);
 app.listen(3000);
