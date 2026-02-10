970f072d70962644088afb76d11643b1e1701e2c
suhaibfida <suhaibfida33@outlook.com>
2026-02-11 02:01:41 +0530

    create document

--- apps/client/src/components/CreateDoc.tsx
+++ apps/client/src/components/CreateDoc.tsx
@@ -1,6 +1,30 @@
 import { Input } from "@repo/ui/Input";
 import { Button } from "@repo/ui/Button";
+import { useRef, useState } from "react";
+import { ApiCreate } from "./api";
 export const CreateDoc = () => {
+  const [loading, setLoading] = useState("Create");
+  const [disabled, setDisabled] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const onClick = async () => {
+    setLoading("creating....");
+    setDisabled(true);
+    if (!inputRef.current) {
+      return;
+    }
+    const value = inputRef.current.value.trim();
+    if (!value) {
+      throw new Error("Error");
+    }
+    try {
+      await ApiCreate({ data: value });
+      console.log();
+      setLoading("Create");
+      setDisabled(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <>
       <div className="flex justify-center items-center h-screen bg-black">
@@ -13,10 +37,17 @@ export const CreateDoc = () => {
             Enter the name of the document:
           </div>
           <div className="pl-7 pt-2 text-white">
-            <Input type="text" placeholder="Title" />
+            <Input
+              ref={inputRef}
+              type="text"
+              placeholder="Title"
+              onKeyDown={(e) => e.key === "Enter" && onClick}
+            />
           </div>
           <div className="pl-7 pt-7 text-white">
-            <Button type="submit">Create</Button>
+            <Button type="submit" onClick={onClick} disabled={disabled}>
+              {loading}
+            </Button>
           </div>
         </div>
       </div>
---
+++ apps/client/src/components/api.ts
@@ -0,0 +1,17 @@
+interface Create {
+  data: string;
+}
+export const ApiCreate = async (data: Create) => {
+  const res = await fetch(
+    "http://localhost:3000/api/v1/dashboard/createdocument",
+    {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(data),
+    },
+  );
+  if (!res) {
+    throw new Error("Document creation failed");
+  }
+  return;
+};
