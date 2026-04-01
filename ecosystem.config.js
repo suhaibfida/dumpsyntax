07cace0a646d0d661baeb8a7d2a574f69eaa4df1
suhaibfida <suhaibfida33@outlook.com>
2026-04-01 23:01:20 +0530

    add config file

---
+++ ecosystem.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  apps: [
+    {
+      name: "api",
+      script: "apps/api/dist/main.js",
+      instances: 2,
+      exec_mode: "cluster",
+      env: {
+        PORT: 4000,
+        FRONTEND_URL: "https://dumpsyntax.suhaibfida.dev",
+        JWT_SECRET: process.env.JWT_SECRET,
+        DATABASE_URL: process.env.DATABASE_URL,
+      },
+    },
+    {
+      name: "socket-server",
+      script: "apps/socket-server/dist/main.js",
+      exec_mode: "fork",
+      env: {
+        PORT: 4001,
+        FRONTEND_URL: "https://dumpsyntax.suhaibfida.dev",
+        JWT_SECRET: process.env.JWT_SECRET,
+        DATABASE_URL: process.env.DATABASE_URL,
+      },
+    },
+  ],
+};
